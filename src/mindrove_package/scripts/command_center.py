74c55846e8e934782311209de06a7626546e552c
Justin Xiang <jx372@cornell.edu>
2024-07-23 11:14:54 -0400

    add robot commands

--- src/mindrove_package/scripts/command_center.py
+++ src/mindrove_package/scripts/command_center.py
@@ -1,28 +1,54 @@
 import rospy
 from std_msgs.msg import String
+from geometry_msgs.msg import PoseStamped
+from sound_play.libsoundplay import SoundClient
 
-def callback(data):
-    command = determine_robot_command(data.data)
-    pub.publish(command)
-
-def determine_robot_command(classification : String):
-    # gestures = ['wave', 'push', 'squeeze', 'point', 'thumb_up']
+def callback(msg):
+    classification = msg.data
     if classification == "wave":
-        print("wave")
+        send_goal(1.0, 2.0, 0.0, 1.0)  # Example coordinates
     elif classification == "push":
-        print("point")
+        send_goal(-1.0, 0.0, 0.0, 1.0)  # Move out of the way
     elif classification == "squeeze":
-        print("squeeze")
+        change_lights(255, 0, 0)  # Change lights to red
     elif classification == "point":
-        print("point")
+        play_sound('/path/to/sound/file.wav')  # Play a sound
     elif classification == "thumb_up":
-        print("thumb_up")
+        play_sound('/path/to/sound/file.wav')  # Play a sound
+
+def send_goal(x, y, z, w):
+    goal = PoseStamped()
+    goal.header.frame_id = "map"
+    goal.header.stamp = rospy.Time.now()
+    goal.pose.position.x = x
+    goal.pose.position.y = y
+    goal.pose.position.z = 0.0
+    goal.pose.orientation.x = 0.0
+    goal.pose.orientation.y = 0.0
+    goal.pose.orientation.z = z
+    goal.pose.orientation.w = w
+    goal_pub.publish(goal)
+
+def change_lights(r, g, b):
+    rospy.wait_for_service('/mm1/led/set_strip_color')
+    try:
+        set_strip_color = rospy.ServiceProxy('/mm1/led/set_strip_color', rospy.SetStripColor)
+        set_strip_color(0, r, g, b)  # Left strip
+        set_strip_color(1, r, g, b)  # Right strip
+    except rospy.ServiceException as e:
+        print("Service call failed: %s" % e)
+
+def play_sound(file_path):
+    soundhandle = SoundClient()
+    rospy.sleep(1)
+    soundhandle.playWave(file_path)
 
 def listener():
     rospy.init_node('command_center', anonymous=True)
     rospy.Subscriber('classification_result', String, callback)
-    global pub
+    global pub, goal_pub
     pub = rospy.Publisher('robot_command', String, queue_size=10)
+    goal_pub = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=10)
     rospy.spin()
 
 if __name__ == '__main__':
